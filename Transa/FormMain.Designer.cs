f6572914cbb12fc99b9355c4b57ad794dbb2c6c6
GabriGat2 <gabri.naba21a@gmail.com>
2025-01-20 18:49:50 +0100

    Generazione versione 1.0.0.9

--- Transa/FormMain.Designer.cs
+++ Transa/FormMain.Designer.cs
@@ -164,7 +164,7 @@ namespace Transa
             this.ClientSize = new System.Drawing.Size(1237, 653);
             this.Controls.Add(this.splitContainer1);
             this.Name = "FormMain";
-            this.Text = "TRANSA: generatore di transizoni - Versione 1.0.0.8";
+            this.Text = "TRANSA: generatore di transizoni - Versione 1.0.0.9";
             this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
